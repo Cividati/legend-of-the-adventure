f293218a111cbb7d65e500c2647248c685dd4790
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-10-22 19:03:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+import struct as stc
 import database as db
 import classes as c
 import random as r
+import time as t
 
 def main():
 
-    #db.create_database()
-
-    class1 = c.clas(1, 'Paladin', 15, 7, 5, 5)
-    class2 = c.clas(2, 'Mage', 7, 3, 10, 3)
-    class3 = c.clas(3, 'Druid', 7, 7, 7, 7)
+    db.create_database()
+    clas =[
+        c.clas(1, 'Paladin', 15, 7, 5, 5),
+        c.clas(2, 'Mage', 7, 3, 10, 3),
+        c.clas(3, 'Druid', 7, 7, 7, 7),
+        c.clas(4, 'Warlock', 5, 2, 10, 3)]
     
-    db.create_class(class1)
-    db.create_class(class2)
-    db.create_class(class3)
+    for cl in clas:
+        db.create_class(cl)
 
-    race1 = c.race(1, 'Human', 5, 5, 5, 5)
-    race2 = c.race(2, 'Elf', 4, 3, 7, 6)
-    race3 = c.race(3, 'Orc', 7, 8, 2, 3)
-    race4 = c.race(4, 'Dwarf', 5, 9, 6, 2)
+    race = [
+        c.race(1, 'Human', 5, 5, 5, 5),
+        c.race(2, 'Elf', 4, 3, 7, 6),
+        c.race(3, 'Orc', 7, 8, 2, 3),
+        c.race(4, 'Dwarf', 5, 9, 6, 2)]
 
-    player1 = c.player(1, 'Rubens', 1, 0, 1, 1, 10, 10, 10, 10, 10, 10)
-    player2 = c.player(2, 'Tavão', 1, 0, 1, 4)
+    for ra in race:
+        db.create_race(ra)
 
+    player = [
+        c.player(1, 'Rubens', 1, 0, 1, 1, 10, 10, 10, 10, 10, 10),
+        c.player(2, 'Tavão', 1, 0, 1, 4),
+        c.player(3, 'Sader', 1, 0, 4, 2)]
 
-    #db.create_player(player1)
+    for pl in player:
+        db.create_player(pl)
+
 main()
