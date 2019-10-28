88e992d31132c6ab625294a2a21fdd6633c042b4
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-10-28 17:13:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def default_values():
     db.create_database()
     clas =[
         c.clas('Paladin', 15, 7, 5, 5),
+        c.clas('Priest', 5, 2, 10, 4),
         c.clas('Mage', 7, 3, 10, 3),
         c.clas('Druid', 7, 7, 7, 7),
         c.clas('Warlock', 5, 2, 10, 3)
@@ -28,9 +29,10 @@ def default_values():
         db.create_race(ra)
 
     player = [
-        c.player('Rubens', 1, 0, 1, 1, 10, 10, 10, 10, 10, 10),
+        c.player('Rubens', 1, 0, 1, 1),
         c.player('Tavão', 1, 0, 1, 4),
-        c.player('Sader', 1, 0, 4, 2)
+        c.player('Sader', 1, 0, 4, 2),
+        c.player('Kpis', 1, 0, 1, 1)
         ]
 
     for pl in player:
@@ -43,71 +45,136 @@ def default_values():
         ]
 
     for it in iten:
-        db.create_iten(it)
-
-default_values()
+        db.create_item(it)
 
 def main():
     os.system('cls')
     print('Welcome to the legend of the adventure!')
-    op = input('0. Exit\n1. Create character\n2. Load character\n3. Credits\nR. ')
-    player = c.player('')
-    os.system('cls')
-    if op == '1':
-        # Creating character
-        
-        print('\nCreating character')
-
-        name = input('Name: ')
-
-        totalC = db.get_all_classes()
-        print()
-        print('Choose your class!')
-        print('id -  name  - con - str - int - spd')
-        for i in totalC:
-            print(i.id,'-',i.name,'-',i.con,'-',i.str,'-',i.int,'-',i.spd)
-
-        clas_id = input('Class id: ')
-
-        totalR = db.get_all_races()
-        print('\nChoose your race!')
-        print('id -  name  - con - str - int - spd')
-        for i in totalR:
-            print(i.id,'-',i.name,'-',i.con,'-',i.str,'-',i.int,'-',i.spd)
-            
-        race_id = input('\nRace id: ')
-
-        p = c.player(name, id_class=clas_id, id_race=race_id)
-        db.create_player(p)
-        db.get_player(p.id)
-        p.show()
-
-        print('\nPlayer created!')
-
-    elif op == '2':
-        # Loading character
-        print('loading character')
-        # Creating character
-        i = 0
-        print('Choose yout character')
-        totalP = db.get_all_players()
-        for i in totalP:
-            print(i.id,'-',i.name)
-
-        op = input('Select your player id: ')
-        player = db.get_player(op)
-
-        print(player.name, 'selected!')
-        player.show()
-
-    elif op == '3':
-        # Credits
+    op = -1
+    while op != '0':
+        op = input('0. Exit\n1. Create character\n2. Load character\n3. Credits\nR. ')
+        player = c.player('')
         os.system('cls')
-        print('This game is a ')
-    
+        if op == '1':
+            # Creating character
+            
+            print('Creating character')
+
+            name = input('Name: ')
+
+            totalC = db.get_all_classes()
+            print()
+            print('Choose your class!')
+            print('id -  name - con - str - int - spd')
+            for i in totalC:
+                print(i.id,'-',i.name,'-',i.con,'-',i.str,'-',i.int,'-',i.spd)
+
+            clas_id = input('Class id: ')
+            print('You have chosed '+ db.get_class(clas_id).name)
+
+            totalR = db.get_all_races()
+            print('\nChoose your race!')
+            print('id -  name - con - str - int - spd')
+            for i in totalR:
+                print(i.id,'-',i.name,'-',i.con,'-',i.str,'-',i.int,'-',i.spd)
+                
+            race_id = input('Race id: ')
+            print('You have chosed '+ db.get_race(race_id).name)
+
+            p = c.player(name, id_class=clas_id, id_race=race_id)
+            db.create_player(p)
+            p = db.get_player(p.name, 'name')
+            p.show()
+            op = input('\nAre u sure?\n1.Yes\n2.No\nR.')
+            if op == '1':
+                os.system('cls')
+                print(p.name+' created!')
+            elif op == '2':
+                db.rm_player(p)
+                os.system('cls')
+                print('Operation aborted')
+
+        elif op == '2':
+            # Loading character
+            print('loading character')
+            # Creating character
+            i = 0
+            print('Choose yout character')
+            totalP = db.get_all_players()
+            for i in totalP:
+                print(i.id,'-',i.name)
+
+            op = input('Select your player id: ')
+            player = db.get_player(op)
+            player.show()
+
+            op = input('\nAre u sure?\n1.Yes\n2.No\nR.')
+            os.system('cls')
+            if op == '1':
+                print(player.name+' selected!')
+                play(player)
+            elif op == '2':
+                print('Operation aborted')
+            
+
+        elif op == '3':
+            # Credits
+            os.system('cls')
+            print('This game is made full of love ♥')
+        
+        elif op == '0':
+            print('see you next time')
+            pass
+
+        else:
+            print('invalid option!')
+
+def play(player):
+    os.system('cls')
+    print('Here you start your adventure!')
+    mod = input('Select your difficult:\n1.Easy\n2.Medium\n3.Hard\nR.')
+
+    if mod == '1':
+        mod = 1
+    elif mod == '2':
+        mod = 0.65
+    elif mod == '3':
+        mod = 0.37
     else:
-        print('see you next time!')
-        pass
+        mod = 1
+    os.system('cls')
+    print('Difficult modifier switch to', mod)
+    op = -1
+    while op != '0':
+        op = input('0.Exit\n1.Stash\n2.Combat\n3.View character\nR.')
+        os.system('cls')
+        if op == '0':
+            exit()
+        elif op =='1':
+            items = db.get_player_items(player)
 
+            if items:
+                
+                print('Welcome to stash!!')
+                print('This is your items:')
+                for i in items:
+                    print(i.id,'-',i.name)
+                op = input ('Select your item to equip: ')      
+                item = db.get_item(op)
+                db.equip_item(player, item)
+                os.system('cls')
+                print(item.name+' equipped!')
+            else:
+                print('You have no items :(\nKill some mobs to get some items')
+        
+        elif op == '2':
+            print('combat building...')
 
+        elif op == '3':
+            player.show()
+            print()
+
+        else:
+            print('invalid option')
 main()
+
