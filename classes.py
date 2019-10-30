6d87d21cfab72010bd1ecffeb9f5f71174b7caaa
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-10-29 21:22:04 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,338 +1,107 @@
-import sqlite3
-import os
-import classes as c
-import pandas as pd
+import random as r
+import time as t
 
-dir_path = os.path.dirname(__file__)
+class player:
 
-db_url = dir_path + '/'
-
-def get_db_path():
-    return dir_path
-
-def get_connection(db_name = 'database.db'):
-    return sqlite3.connect(db_url + db_name)
-
-def create_database():
-    conn = get_connection()
-    cursor = conn.cursor()
-    # creating the databse 
-
-    try:
-        # Create table item
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS item(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name INTEGER NOT NULL,
-            description CHAR NOT NULL,
-            con FLOAT NOT NULL,
-            str FLOAT NOT NULL,
-            int FLOAT NOT NULL,
-            spd FLOAT NOT NULL
-        );
-        """)
-    except sqlite3.Error as e:
-        print('item error:',e)
-
-    try:
-        # Create table race
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS race(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name INTEGER NOT NULL,
-            con FLOAT NOT NULL,
-            str FLOAT NOT NULL,
-            int FLOAT NOT NULL,
-            spd FLOAT NOT NULL
-        );
-        """)
-    except sqlite3.Error as e:
-        print('race error:',e)
-
-    try:
-        # Create table class
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS class(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name INTEGER NOT NULL,
-            con FLOAT NOT NULL,
-            str FLOAT NOT NULL,
-            int FLOAT NOT NULL,
-            spd FLOAT NOT NULL
-        ); 
-        """)
-    except sqlite3.Error as e:
-        print('races error:',e)
-
-    try:
-        # Create table player
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS player(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name CHAR(45) NOT NULL,
-            level int NOT NULL,
-            exp FLOAT NOT NULL,
-            id_class INT NOT NULL,
-            id_race INT NOT NULL,
-            life FLOAT NOT NULL,
-            mana FLOAT NOT NULL,
-            con FLOAT NOT NULL,
-            str FLOAT NOT NULL,
-            int FLOAT NOT NULL,
-            spd FLOAT NOT NULL,
-            item_equipped INT DEFAULT 0,
-
-            FOREIGN KEY (item_equipped) REFERENCES item(id),
-            FOREIGN KEY (id_race) REFERENCES race(id),
-            FOREIGN KEY (id_class) REFERENCES class(id)
-        );
-        """)
-    except sqlite3.Error as e:
-        print('player error:',e)
-
-    try:
-        # Create table storage
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS inventory(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            id_item INTEGER NOT NULL,
-            id_player INTEGER NOT NULL,
-
-            FOREIGN KEY (id_item) REFERENCES item(id),
-            FOREIGN KEY (id_player) REFERENCES player(id)
-        );
-        """)
-    except sqlite3.Error as e:
-        print('storage error:',e)
-        
-    conn.close()
-
-def create_race(race):
-    conn = get_connection()
-
-    conn.execute(f"INSERT INTO race(name, con, str, int, spd) VALUES( "
-        f"'{race.name}', '{race.con}', '{race.str}', '{race.int}', '{race.spd}');")
-    conn.commit()
-    conn.close()    
-
-def create_class(clas):
-    conn = get_connection()
-
-    conn.execute(f"INSERT INTO class(name, con, str, int, spd) VALUES( "
-        f"'{clas.name}', '{clas.con}', '{clas.str}', '{clas.int}', '{clas.spd}');")
-    conn.commit()
-    conn.close() 
-
-def create_item(item):
-    conn = get_connection()
-
-    conn.execute(f"INSERT INTO item(name, description, con, str, int, spd) VALUES( "
-        f"'{item.name}', '{item.description}', '{item.con}', '{item.str}', '{item.int}', '{item.spd}');")
-    conn.commit()
-    conn.close() 
-
-def create_player(player):
-    conn = get_connection() 
+    def __init__(self, name, level = 1, exp = 0, id_class = 1, 
+    id_race = 1, life = 10, mana = 10  , con = r.randint(1,10),
+    Str = r.randint(1,10), Int = r.randint(1,10), 
+    spd = r.randint(1,10), item_equipped = 0, iD = 0):
     
-    player.life += get_stats('race', 'con', player.id_race)*0.5 + get_stats('class', 'con', player.id_class)*0.5
-    player.mana += get_stats('race', 'int', player.id_race)*0.5 + get_stats('class', 'int', player.id_class)*0.5
-    player.con +=  get_stats('race', 'con', player.id_race) + get_stats('class', 'con', player.id_class)
-    player.str += get_stats('race', 'str', player.id_race) + get_stats('class', 'str', player.id_class)
-    player.int += get_stats('race', 'int', player.id_race) + get_stats('class', 'int', player.id_class)
-    player.spd += get_stats('race', 'spd', player.id_race) + get_stats('class', 'spd', player.id_class)
-    
-    conn.execute(f"INSERT INTO player(name, level, exp, id_class, id_race, life, mana, con, str, int, spd) VALUES(" 
-        f"'{player.name}', '{player.level}', '{player.exp}', '{player.id_class}', '{player.id_race}',"
-        f"'{player.life}', '{player.mana}', '{player.con}', '{player.str}', '{player.int}', '{player.spd}');")
-    conn.commit()
-    conn.close()    
+        self.id = iD
+        self.name  = name
+        self.level = level
+        self.exp = exp
+        self.id_class = id_class
+        self.id_race = id_race
+        self.life = life
+        self.mana = mana
+        self.con = con
+        self.str = Str
+        self.int = Int
+        self.spd = spd
+        self.item_equipped = item_equipped
 
-def get_stats(tableName, stat, idName):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"SELECT {stat} FROM {tableName} WHERE id = {idName};")
-    
-    rows = cur.fetchall()
-    value = rows[0][0]
+    def show(self):
+        import database as db
 
-    return value
+        print('\nTake a look in your character...\n')
+        print('Name:', self.name)
+        print('Level:', self.level)
+        print('Exp:', self.exp)
+        print('Class:', db.get_class(self.id_class).name)
+        print('Race:', db.get_race(self.id_race).name)
+        print('Life:', self.life)
+        print('Mana:', self.mana)
+        print('Con:', self.con)
+        print('Str:', self.str)
+        print('Int:', self.int)
+        print('Spd:', self.spd)
 
-def get_item(value = '', param = 'id'):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM item WHERE {param} = "{value}";')
-    
-    rows = cur.fetchall()
+    def fight(self, enemy):
+        import os
+        import database as db
+        print('\nA wild '+enemy.name+' appears!')
 
-    if rows:
-        item = c.item(
-            rows[0][1],
-            rows[0][2],
-            rows[0][3],
-            rows[0][4],
-            rows[0][5],
-            rows[0][6],
-            rows[0][0]
-        )
-        return item
-    return False
+        while enemy.life > 0:
+            #os.system('cls')
+            stat = input('Use\n1.Str\n2.Int\n3.Spd\nR.')
+            if stat == '1':
+                stat = self.str
+            elif stat == '2':
+                stat = self.int
+            elif stat == '3':
+                stat = self.spd
+            else: stat = self.str  
 
-def get_player(value = '', param = 'id'):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM player WHERE {param} = "{value}";')
-    
-    rows = cur.fetchall()
+            print('\nYou try to attack '+enemy.name+':'+str(enemy.life))
 
-    if rows:
-        player = c.player(
-            rows[0][1],
-            rows[0][2],
-            rows[0][3],
-            rows[0][4],
-            rows[0][5],
-            rows[0][6],
-            rows[0][7],
-            rows[0][8],
-            rows[0][9],
-            rows[0][10],
-            rows[0][11],
-            rows[0][11],
-            rows[0][0]
-        )
-        return player
-    return False
+            dice = r.randint(1,20)
+            print('You roll: '+str(dice))
+            if dice >= enemy.con:
+                totalHit = stat
+                
+                print('You hit:', (totalHit)) 
+                enemy.life = enemy.life - totalHit
+            else: print('You miss the attack!')
+        print('You win '+str(enemy.exp)+' exp!')
+        self.exp += self.exp + enemy.exp
+        db.update_player(self)
 
-def get_class(value = '', param = 'id'):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM class WHERE {param} = "{value}";')
-    
-    rows = cur.fetchall()
+class race:
 
-    if rows:
-        clas = c.clas(
-            rows[0][1],
-            rows[0][2],
-            rows[0][3],
-            rows[0][4],
-            rows[0][5],
-            rows[0][0]
-        )
-        return clas
-    return False
+    def __init__(self, name, con = r.randint(1,10), 
+    Str =  r.randint(1,10), Int =  r.randint(1,10),
+    spd =  r.randint(1,10), iD = 0):
+        self.id = iD
+        self.name = name
+        self.con = con
+        self.str = Str
+        self.int = Int
+        self.spd = spd
 
-def get_race(value = '', param = 'id'):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM race WHERE {param} = "{value}";')
-    
-    rows = cur.fetchall()
+class clas:
 
-    if rows:
-        race = c.race(
-            rows[0][1],
-            rows[0][2],
-            rows[0][3],
-            rows[0][4],
-            rows[0][5],
-            rows[0][0]
-        )
-        return race
-    return False
+    def __init__(self, name, con = r.randint(1,10), 
+    Str =  r.randint(1,10), Int =  r.randint(1,10),
+    spd =  r.randint(1,10), iD = 0):
+        self.id = iD
+        self.name = name
+        self.con = con
+        self.str = Str
+        self.int = Int
+        self.spd = spd
 
-def get_all_players():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM player;')
-    rows = cur.fetchall()
-    p = []
-    for i in rows:
-        player = get_player(i[0])
-        p.append(player)
-    return p
+class item:
 
-def get_all_classes():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM class;')
-    rows = cur.fetchall()
-    c = []
-    for i in rows:
-        clas = get_class(i[0])
-        c.append(clas)
-    return c
-    
-def get_all_races():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM race;')
-    rows = cur.fetchall()
-    r = []
-    for i in rows:
-        race = get_race(i[0])
-        r.append(race)
-    return r
-
-def get_player_items(player):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM inventory WHERE id_player = {player.id};')
-    rows = cur.fetchall()
-    it = []
-    for i in rows:
-        item = get_item(i[1])
-        it.append(item)
-    return it
-
-def update_player(player):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f'UPDATE player SET'
-                f' name = "{player.name}",'
-                f' level = "{player.level}",'
-                f' exp = "{player.exp}",'
-                f' id_class = "{player.id_class}",'
-                f' id_race = "{player.id_race}",'
-                f' life = "{player.life}",'
-                f' mana = "{player.mana}",'
-                f' con = "{player.con}",'
-                f' str = "{player.str}",'
-                f' int = "{player.int}",'
-                f' spd = "{player.spd}",'
-                f' item_equipped = "{player.item_equipped}"'
-                f' WHERE id = "{player.id}";')
-    conn.commit()
-
-def rm_player(player):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"DELETE FROM player WHERE id = {player.id};")
-    conn.commit()
-    
-def update_stats(tableName, stat, value, idName):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"UPDATE {tableName} SET {stat} = {value} WHERE id = {idName};")
-    conn.commit()
-
-def att_item(player, item):
-    # attribute a item to a player
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"INSERT INTO inventory(id_player, id_item) VALUES("
-    f"{player.id},{item.id});")
-    conn.commit()
-
-def rm_item(player, item):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"DELETE FROM inventory WHERE "
-    f"id_player = {player.id} AND id_item = {item.id};")
-    conn.commit()
-
-def equip_item(player, item):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(f"UPDATE player SET item_equipped = {item.id} WHERE id = {player.id};")
-    conn.commit()
+    def __init__(self, name, description = '',
+    con = r.randint(1,10), Str =  r.randint(1,10), 
+    Int =  r.randint(1,10), spd =  r.randint(1,10),
+    iD = 0):
+        self.id = iD
+        self.name = name
+        self.description = description
+        self.con = con
+        self.str = Str
+        self.int = Int
+        self.spd = spd
